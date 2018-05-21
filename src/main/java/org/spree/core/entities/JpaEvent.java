bfcd79e36715f7eb6e03333a241ca05056eeb123
kudndre <andreykudr@yahoo.com>
2018-05-21 23:06:41 +0300

    culture event. add members count

--- src/main/java/org/spree/core/entities/JpaEvent.java
+++ src/main/java/org/spree/core/entities/JpaEvent.java
@@ -31,7 +31,7 @@ public class JpaEvent implements Event {
     private String systemId;
     @Column(name = "members_count")
     private int membersCount;
-    @OneToMany(targetEntity = JpaCategory.class, cascade = CascadeType.ALL)
+    @ManyToMany(targetEntity = JpaCategory.class, cascade = CascadeType.REMOVE)
     private List<JpaCategory> categories;
 
     public JpaEvent() {
